60b88a48e3b84d828209655908d0fc03d8a83642
jtrembac <34035666+jtrembac@users.noreply.github.com>
2017-11-28 19:30:33 -0600

    Finished
    Finished Assignment

--- app/src/main/java/com/example/jeff/ece354jefftrembackicalc/MainActivity.java
+++ app/src/main/java/com/example/jeff/ece354jefftrembackicalc/MainActivity.java
@@ -6,16 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Stack;
 import java.lang.*;
 
-
-
-
 public class MainActivity extends AppCompatActivity {
-
-
 
     //assignment variables
     TextView result;
@@ -23,24 +18,17 @@ public class MainActivity extends AppCompatActivity {
     Button plus, minus, asterisk, slash, power, root, clear;
 
     double result_num;
-    int op1, op2;
     double op3, op4;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         //assignment stuff
         result = (TextView)findViewById(R.id.result);
         operand1 = (EditText)findViewById(R.id.operand1);
         operand2 = (EditText)findViewById(R.id.operand2);
-
-
 
         //assignment stuff
         plus = (Button)findViewById(R.id.plus);
@@ -51,48 +39,15 @@ public class MainActivity extends AppCompatActivity {
         power = (Button)findViewById(R.id.power);
         clear = (Button)findViewById(R.id.clear);
 
+        final EvaluateString opstring = new EvaluateString();
 
         //assignment stuff
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                String exp = operand1.getText().toString();
-
-                Stack<String> ops = new Stack<String>();
-                Stack<Double> vals = new Stack<Double>();
-
-                for(int i = 0; i < exp.length; i++) {
-                    String s = exp[i];
-                    if (s.equals("(")) {
-                    }
-                    else if (s.equals("+") || s.equals("*")) {
-                        ops.push(s);
-                    } else if (s.equals(")")) {
-                        getComp(ops, vals);
-                    } else {
-                        vals.push(Double.parseDouble(s));
-                    }
-                }
-                getComp(ops, vals);
-                System.out.println(vals.pop());
-            }
-
-                private static void getComp(Stack<String> ops, Stack<Double> vals) {
-                    String op = ops.pop();
-                    if (op.equals("+")) {
-                        vals.push(vals.pop() + vals.pop());
-                    } else if (op.equals("*")) {
-                        vals.push(vals.pop() * vals.pop());
-                    }
-                }
-            }
-
-                */
-
-                op1 = Integer.parseInt(operand1.getText().toString());
-                op2 = Integer.parseInt(operand2.getText().toString());
-                result_num = op1 + op2;
+                op3 = opstring.evaluate(operand1.getText().toString());
+                op4 = opstring.evaluate(operand2.getText().toString());
+                result_num = op3 + op4;
                 result.setText(String.valueOf(result_num));
             }
         });
@@ -100,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op1 = Integer.parseInt(operand1.getText().toString());
-                op2 = Integer.parseInt(operand2.getText().toString());
-                result_num = op1 - op2;
+                op3 = opstring.evaluate(operand1.getText().toString());
+                op4 = opstring.evaluate(operand2.getText().toString());
+                result_num = op3 - op4;
                 result.setText(String.valueOf(result_num));
             }
         });
@@ -110,9 +65,9 @@ public class MainActivity extends AppCompatActivity {
         asterisk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op1 = Integer.parseInt(operand1.getText().toString());
-                op2 = Integer.parseInt(operand2.getText().toString());
-                result_num = op1 * op2;
+                op3 = opstring.evaluate(operand1.getText().toString());
+                op4 = opstring.evaluate(operand2.getText().toString());
+                result_num = op3 * op4;
                 result.setText(String.valueOf(result_num));
             }
         });
@@ -120,13 +75,14 @@ public class MainActivity extends AppCompatActivity {
         slash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op1 = Integer.parseInt(operand1.getText().toString());
-                op2 = Integer.parseInt(operand2.getText().toString());
-                if(op2 == 0){
+                op3 = opstring.evaluate(operand1.getText().toString());
+                op4 = opstring.evaluate(operand2.getText().toString());
+                if(op4 == 0.0){
                     result.setText("Error: division by 0");
+                    android.widget.Toast.makeText(MainActivity.this, "Error: Division by zero not allowed!", android.widget.Toast.LENGTH_LONG).show();
                 }
                 else{
-                    result_num = op1 / op2;
+                    result_num = op3 / op4;
                     result.setText(String.valueOf(result_num));
                 }
             }
@@ -136,10 +92,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(operand1 != null && operand1.getText() != null) {
-                    op3 = Double.parseDouble(operand1.getText().toString());
+                    op3 = opstring.evaluate(operand1.getText().toString());
                 }
-                if(op3 < 0){
+                if(op3 < 0.0){
                     result.setText("Error: Operand 1 must be positive");
+                    android.widget.Toast.makeText(MainActivity.this, "Error: Operand 1 must be postive!", android.widget.Toast.LENGTH_LONG).show();
                 }
                 else {
                     result_num = java.lang.Math.pow(op3, 0.5);
@@ -152,10 +109,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(operand1 != null && operand1.getText() != null) {
-                    op3 = Double.parseDouble(operand1.getText().toString());
+                    op3 = opstring.evaluate(operand1.getText().toString());
+
                 }
                 if(operand2 != null && operand2.getText() != null) {
-                    op4 = Double.parseDouble(operand2.getText().toString());
+                    op4 = opstring.evaluate(operand2.getText().toString());
                 }
                 result_num = java.lang.Math.pow(op3, op4);
                 result.setText(String.valueOf(result_num));
